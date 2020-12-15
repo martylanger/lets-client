371aea7f78c781bab3c848ee0ba5479f0bdadcaa
martylanger <langer.marty@gmail.com>
2020-12-14 22:27:42 -0500

    Arrange ElectionForm form elements vertically
    and import bootstrap to put the buttons in a separate column

--- src/components/shared/ElectionForm.js
+++ src/components/shared/ElectionForm.js
@@ -1,43 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const ElectionForm = ({ election, handleSubmit, handleChange, cancelPath }) => (
   <form onSubmit={handleSubmit}>
-    <label>Name</label>
-    <input
-      placeholder="My Election"
-      value={election.name}
-      name="name"
-      onChange={handleChange}
-    />
+    <Container>
+      <Row>
+        <Col>
+          <label>Name </label>
+          <input
+            placeholder="My Election"
+            value={election.name}
+            name="name"
+            onChange={handleChange}
+          />
+          <p></p>
+          <label>Voting method </label>
+          <select
+            name="voting_method"
+            value={election.voting_method}
+            onChange={handleChange}
+          >
+            <option value="">--Please choose an option--</option>
+            <option value="instant-runoff">Instant runoff</option>
+            <option value="borda-count">Borda count</option>
+            <option value="plurality">Plurality</option>
+            <option value="approval">Approval</option>
+          </select>
+          <p></p>
+          <label>Description </label>
+          <input
+            placeholder="What should we do?"
+            value={election.description}
+            name="description"
+            onChange={handleChange}
+          />
+        </Col>
+        <Col>
+          <Button variant="primary" type="submit">Submit</Button>
 
-    <label>Voting method</label>
-    <select
-      name="voting_method"
-      value={election.voting_method}
-      onChange={handleChange}
-    >
-      <option value="">--Please choose an option--</option>
-      <option value="instant-runoff">Instant runoff</option>
-      <option value="borda-count">Borda count</option>
-      <option value="plurality">Plurality</option>
-      <option value="approval">Approval</option>
-    </select>
-
-    <label>Description</label>
-    <input
-      placeholder="What should we do?"
-      value={election.description}
-      name="description"
-      onChange={handleChange}
-    />
-
-    <Button variant="primary" type="submit">Submit</Button>
-
-    <Link to={cancelPath}>
-      <Button variant="secondary">Cancel</Button>
-    </Link>
+          <Link to={cancelPath}>
+            <Button variant="secondary">Cancel</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
   </form>
 )
 
