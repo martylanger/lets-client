697ccd9e78aeae1a79788aefe8adfdef280d5276
martylanger <langer.marty@gmail.com>
2021-01-01 19:27:14 -0500

    Reimplement plurality voting
    Set up switch in Election for delivering results from the correct
      voting method component
    Move that logic inside the else statement right before electionJSX =
    Uncomment importation of Plurality component

--- src/components/shared/Plurality.js
+++ src/components/shared/Plurality.js
@@ -4,6 +4,8 @@ import doTally from '../../modules/doTally'
 import mostVotes from '../../modules/mostVotes'
 
 const Plurality = props => {
+  console.log(ballotsToArray(props.election.ballots))
+  console.log(doTally(ballotsToArray(props.election.ballots)))
   const results = mostVotes(doTally(ballotsToArray(props.election.ballots)))
   const winner = results.length > 1 ? 'Winners' : 'Winner'
   return (
