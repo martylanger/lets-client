654cd85d2bd93d865d4b85bc6c8f5c0bc910b050
martylanger <langer.marty@gmail.com>
2021-01-03 18:53:02 -0500

    Fix bug causing crash if no ballots in Plurality
    Make Plurality logic only run if a ballot has been submitted

--- src/components/shared/Plurality.js
+++ src/components/shared/Plurality.js
@@ -4,13 +4,26 @@ import doTally from '../../modules/doTally'
 import mostVotes from '../../modules/mostVotes'
 
 const Plurality = props => {
-  const tally = mostVotes(doTally(ballotsToArray(props.election.ballots)))
-  const results = tally.map(victor => props.election.choices[victor - 1].title)
-  const winner = results.length > 1 ? 'Winners' : 'Winner'
+  let resultsJSX = null
+
+  if (props.election.ballots.length > 0) {
+    const tally = mostVotes(doTally(ballotsToArray(props.election.ballots)))
+    const results = tally.map(victor => (
+      <li key={victor.id}>
+        {props.election.choices[victor - 1].title}
+      </li>
+    ))
+    const winner = results.length > 1 ? 'Winners' : 'Winner'
+    resultsJSX = (
+      <React.Fragment>
+        <p>{winner}:</p>
+        {results}
+      </React.Fragment>
+    )
+  }
+
   return (
-    <React.Fragment>
-      <p>{winner}: {results}</p>
-    </React.Fragment>
+    resultsJSX
   )
 }
 
