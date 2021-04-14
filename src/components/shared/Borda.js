b3073b3a750343b9d91947c7a850ee16b1463a56
martylanger <langer.marty@gmail.com>
2021-04-14 19:10:07 -0400

    Draft Borda count logic
    Compose Borda count logic
    Place it in voting methods template

--- src/components/shared/Borda.js
+++ src/components/shared/Borda.js
@@ -6,26 +6,39 @@ import mostVotes from '../../modules/mostVotes'
 const Borda = props => {
   // DETERMINE THE WINNER USING A BORDA COUNT
 
-  // For each ballot, give options.length-1 points to the top choice,
-  //  options.length-2 points to the second choice, and so on.
+  // For each ballot, give numChoices-1 points to the top choice,
+  //  numChoices-2 points to the second choice, and so on.
 
-  // Prepare a results array
-  // In the results array, the index # === the option #,
-  //  and the value at the index is the option's total points
+  // Prepare a totalScores array
+  // In the totalScores array, the index # === the choice #,
+  //  and the value at the index is the choice's total points
+  const numChoices = props.election.choices.length
+  const borda = function (ballots) {
+    // Create a new array of the ballots' selections with the strings converted to arrays
+    const ballotsArray = ballotsToArray(ballots)
 
-
+    const totalScores = Array(numChoices + 1).fill(0)
+    // props.election.choices.forEach(choice => {
+    //   results[choice] = 0
+    // })
+    let tally
+    for (let i = 0; i < numChoices; i++) {
+      tally = doTally(ballotsArray, i)
+      for (let j = 0; j < tally.length; j++) {
+        totalScores[i] += tally[i] * (numChoices - j)
+      }
+    }
+    return mostVotes(totalScores)
+  }
 
   let resultsJSX = null
 
   if (props.election.ballots.length > 0) {
-    const tally = mostVotes(doTally(ballotsToArray(props.election.ballots)))
-    const results = tally.map(victor => (
+    const results = borda(props.election.ballots).map(victor => (
       <li key={victor.toString()}>
         {props.election.choices[victor - 1].title}
       </li>
     ))
-
-
     const winner = results.length > 1 ? 'Winners' : 'Winner'
     resultsJSX = (
       <React.Fragment>
