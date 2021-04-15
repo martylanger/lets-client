78927546ebac3161a61542a565de3106b27af8ab
martylanger <langer.marty@gmail.com>
2021-04-15 19:21:23 -0400

    Annotate Borda

--- src/components/shared/Borda.js
+++ src/components/shared/Borda.js
@@ -7,25 +7,27 @@ const Borda = props => {
   // DETERMINE THE WINNER USING A BORDA COUNT
 
   // For each ballot, give numChoices-1 points to the top choice,
-  //  numChoices-2 points to the second choice, and so on.
+  //  numChoices-2 points to the second choice, and so on
+  // Last place is omitted from the loop below because it scores 0 points
 
-  // Prepare a totalScores array
-  // In the totalScores array, the index # === the choice #,
-  //  and the value at the index is the choice's total points
   const numChoices = props.election.choices.length
   const borda = function (ballots) {
     // Create a new array of the ballots' selections with the strings converted to arrays
     const ballotsArray = ballotsToArray(ballots)
 
+    // Prepare a totalScores array
+    // In the totalScores array, the index # === the choice #,
+    //  and the value at the index is the choice's total points
     const totalScores = Array(numChoices + 1).fill(0)
-    // props.election.choices.forEach(choice => {
-    //   results[choice] = 0
-    // })
+
     let tally
-    for (let i = 0; i < numChoices; i++) {
+
+    // At each rank, tally the votes at that rank (E.g. all the first-place choices)
+    // For each vote at a given rank, a choice gets numChoices-rank-1 points
+    for (let i = 0; i < numChoices - 1; i++) {
       tally = doTally(ballotsArray, i)
       for (let j = 0; j < tally.length; j++) {
-        totalScores[i] += tally[i] * (numChoices - j)
+        totalScores[j] += tally[j] * (numChoices - i - 1)
       }
     }
     return mostVotes(totalScores)
