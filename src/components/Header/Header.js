877d5236cbabca5f184aba3964b05bae71ac7611
martylanger <langer.marty@gmail.com>
2021-04-21 15:56:23 -0400

    Introduce color palette
    Define five colors ($a-$e) in index.scss
    Add .navbar background-color: $b
    Remove bg="primary" from Header Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar variant="dark" expand="md">
     <Navbar.Brand href="#">
       Let&#39;s
     </Navbar.Brand>
