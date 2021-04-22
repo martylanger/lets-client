998126740e7a6214175fd99db27aff0e54cac920
martylanger <langer.marty@gmail.com>
2021-04-22 13:33:00 -0400

    Change ElectionForm to bootstrap form
    Was a plain old HTML form
    Import React Form
    Replace rows and cols with divs with appropriate classnames

--- src/components/shared/ElectionForm.js
+++ src/components/shared/ElectionForm.js
@@ -1,25 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 
 const ElectionForm = ({ election, handleSubmit, handleChange, cancelPath }) => (
-  <form onSubmit={handleSubmit}>
-    <Container>
-      <Row>
-        <Col>
-          <label>Name </label>
-          <input
-            placeholder="My Election"
-            value={election.name}
+  <div className="row">
+    <div className="col-sm-10 col-md-8 mx-auto mt-5">
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            required
+            type="text"
             name="name"
+            value={election.name}
+            placeholder="Election name"
             onChange={handleChange}
           />
-          <p></p>
-          <label>Voting method </label>
-          <select
-            name="voting_method"
-            value={election.voting_method}
+        </Form.Group>
+        <Form.Group controlId="voting-method">
+          <Form.Label>Voting method</Form.Label>
+          <Form.Control
+            required
+            name="voting-method"
+            value={election.votingMethod}
+            as="select"
             onChange={handleChange}
           >
             <option value="">--Please choose an option--</option>
@@ -27,26 +32,72 @@ const ElectionForm = ({ election, handleSubmit, handleChange, cancelPath }) => (
             <option value="borda-count">Borda count</option>
             <option value="plurality">Plurality</option>
             <option value="approval" disabled>Approval</option>
-          </select>
-          <p></p>
-          <label>Description </label>
-          <input
-            placeholder="What should we do?"
-            value={election.description}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="description">
+          <Form.Label>Description (optional)</Form.Label>
+          <Form.Control
+            type="text"
             name="description"
+            value={election.description}
+            placeholder="What should we do?"
             onChange={handleChange}
           />
-        </Col>
-        <Col>
-          <Button variant="primary" type="submit">Submit</Button>
-
-          <Link to={cancelPath}>
-            <Button variant="secondary">Cancel</Button>
-          </Link>
-        </Col>
-      </Row>
-    </Container>
-  </form>
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  </div>
 )
+
+// <form onSubmit={handleSubmit}>
+//     <Container>
+//       <Row>
+//         <Col>
+//           <label>Name </label>
+//           <input
+//             placeholder="My Election"
+//             value={election.name}
+//             name="name"
+//             onChange={handleChange}
+//           />
+//           <p></p>
+//           <label>Voting method </label>
+//           <select
+//             name="voting_method"
+//             value={election.voting_method}
+//             onChange={handleChange}
+//           >
+//             <option value="">--Please choose an option--</option>
+//             <option value="instant-runoff">Instant runoff</option>
+//             <option value="borda-count">Borda count</option>
+//             <option value="plurality">Plurality</option>
+//             <option value="approval" disabled>Approval</option>
+//           </select>
+//           <p></p>
+//           <label>Description </label>
+//           <input
+//             placeholder="What should we do?"
+//             value={election.description}
+//             name="description"
+//             onChange={handleChange}
+//           />
+//         </Col>
+//         <Col>
+//           <Button variant="primary" type="submit">Submit</Button>
+//
+//           <Link to={cancelPath}>
+//             <Button variant="secondary">Cancel</Button>
+//           </Link>
+//         </Col>
+//       </Row>
+//     </Container>
+//   </form>
+// )
 
 export default ElectionForm
