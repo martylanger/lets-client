db5cfeee4934ffeb4ea3c1a413d5e09faa11876d
martylanger <langer.marty@gmail.com>
2021-05-06 11:47:14 -0400

    Number the Choices for reference in Ballots
    Add an index, i, to the Choices map
    Numbers are right-floated and text-muted

--- src/components/shared/Choices.js
+++ src/components/shared/Choices.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, ListGroup } from 'react-bootstrap'
 
 const Choices = props => {
-  const electionChoices = props.election.choices.map((choice) => (
+  const electionChoices = props.election.choices.map((choice, i) => (
     <ListGroup.Item variant='secondary' key={choice.id}>
       {
         props.deletable &&
@@ -15,6 +15,7 @@ const Choices = props => {
         </Button>
       }
       {choice.title}
+      <div className="counter text-muted">#{i + 1}</div>
     </ListGroup.Item>
   ))
 
@@ -29,13 +30,3 @@ const Choices = props => {
 }
 
 export default Choices
-
-// VARIANT WITH NUMBERED OPTIONS
-//
-// const electionChoices = props.election.choices.map((choice, i) => (
-//   <ListGroup.Item action variant='secondary' key={choice.id}>
-//     {props.deletable && <Button variant="danger" onClick={props.handleDestroyChoice} name={choice.id}>Delete</Button>
-//     }
-//     Option #{i + 1}: {choice.title}
-//   </ListGroup.Item>
-// ))
