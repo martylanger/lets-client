c760edb3a6f97d0a2444c52eb35bf852ec10f115
martylanger <langer.marty@gmail.com>
2021-05-11 20:25:07 -0400

    Restyle AllElections with Bootstrap Card
    Add logo
    Add loading Spinner (import Spinner)
    Assign allElectionsJSX with conditionals instead of writing out JSX in
      return statement
    Assign allElectionsJSX a <Redirect> instead of rendering a list of
      <Link>s
    Import Redirect
    Use useState hook for electionId and define handleClick() to use in
      <ListGroup.Item>s

--- src/components/AllElections/AllElections.js
+++ src/components/AllElections/AllElections.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
-import { Card, CardDeck, ListGroup } from 'react-bootstrap'
+import { Card, ListGroup, Spinner } from 'react-bootstrap'
 
 const AllElections = props => {
   const [elections, setElections] = useState([])
+  const [electionId, setElectionId] = useState(null)
+
   useEffect(() => {
     axios({
       url: `${apiUrl}/elections`,
@@ -21,19 +23,48 @@ const AllElections = props => {
       })
   }, [])
 
+  const handleClick = (electionId) => {
+    setElectionId(electionId)
+  }
+
+  // Create a link for each election
   const electionsLinks = elections.map(election => (
-    <li key={election.id}>
-      <Link to={`/elections/${election.id}`}>{election.name}</Link>
-    </li>
+    <ListGroup.Item action className='election-list' key={election.id} onClick={() => handleClick(election.id)} >
+      {election.name}
+    </ListGroup.Item>
   ))
 
+  let allElectionsJSX
+
+  if (!elections) {
+    // If it's loading, give a loading gif
+    allElectionsJSX = (
+      <React.Fragment>
+        <h1>Let&#39;s</h1>
+        <Spinner className="m-auto" animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </React.Fragment>
+    )
+  } else if (electionId) {
+    // Upon user clicking an election, redirect to the election
+    allElectionsJSX = <Redirect to={`/elections/${electionId}`} />
+  } else {
+    allElectionsJSX = (
+      <React.Fragment>
+        <div className="logo-big">Let&#39;s</div>
+        <h4>All Elections</h4>
+        <Card>
+          <ListGroup>
+            {electionsLinks}
+          </ListGroup>
+        </Card>
+      </React.Fragment>
+    )
+  }
+
   return (
-    <React.Fragment>
-      <h4>All Elections</h4>
-      <ul>
-        {electionsLinks}
-      </ul>
-    </React.Fragment>
+    allElectionsJSX
   )
 }
 
