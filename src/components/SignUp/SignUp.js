d20446f90bf1f8daf1bf7b81c7f952d292df49a7
martylanger <langer.marty@gmail.com>
2021-05-26 21:54:26 -0400

    Standardize formatting between SignIn and SignUp

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,8 +4,7 @@ import { withRouter } from 'react-router-dom'
 import { signUp, signIn } from '../../api/auth'
 import messages from '../AutoDismissAlert/messages'
 
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 
 class SignUp extends Component {
   constructor () {
@@ -52,49 +51,51 @@ class SignUp extends Component {
     return (
       <React.Fragment>
         <div className="logo-big">Let&#39;s</div>
-        <div className="row auth-container">
-          <div className="col-sm-10 col-md-8 mx-auto mt-5">
-            <h3>Sign Up</h3>
-            <Form onSubmit={this.onSignUp}>
-              <Form.Group controlId="email">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  required
-                  type="email"
-                  name="email"
-                  value={email}
-                  placeholder="Enter email"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  required
-                  name="password"
-                  value={password}
-                  type="password"
-                  placeholder="Password"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="passwordConfirmation">
-                <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control
-                  required
-                  name="passwordConfirmation"
-                  value={passwordConfirmation}
-                  type="password"
-                  placeholder="Confirm Password"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </div>
-        </div>
+        <Container className="auth-container">
+          <Row>
+            <Col className="col-sm-10 col-md-8 mx-auto mt-5">
+              <h3>Sign Up</h3>
+              <Form onSubmit={this.onSignUp}>
+                <Form.Group controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    required
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Enter email"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Form.Group controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    required
+                    name="password"
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Form.Group controlId="passwordConfirmation">
+                  <Form.Label>Password Confirmation</Form.Label>
+                  <Form.Control
+                    required
+                    name="passwordConfirmation"
+                    value={passwordConfirmation}
+                    type="password"
+                    placeholder="Confirm Password"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
       </React.Fragment>
     )
   }
