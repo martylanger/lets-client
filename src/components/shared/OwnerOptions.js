55026359baef4a9d2edd328b2ca132c20884bb31
Marty <langer.marty@gmail.com>
2022-04-10 23:27:51 -0400

    Conditionally render Edit election and Add Choice
    Group "Edit election" and "Add Choice" buttons with "End voting" button
      in <React.Fragment>, conditionally rendered with props.electionIsOpen

--- src/components/shared/OwnerOptions.js
+++ src/components/shared/OwnerOptions.js
@@ -12,17 +12,18 @@ const OwnerOptions = props => {
   // Check if the user is the owner of the election
   const ownerOptions = props.user.email !== props.election.user.email ? null : (
     <React.Fragment>
-      <Link to={`/elections/${props.match.params.id}/choice-create`}>
-        <Button variant="light">Add an option</Button>
-      </Link>
-
-      <Link to={`/elections/${props.match.params.id}/edit`}>
-        <Button variant="light">Edit election</Button>
-      </Link>
-
       {
-        props.electionIsOpen &&
-        <Button onClick={props.onCloseElection}>End voting</Button>
+        props.electionIsOpen && <React.Fragment>
+          <Link to={`/elections/${props.match.params.id}/choice-create`}>
+            <Button variant="light">Add an option</Button>
+          </Link>
+
+          <Link to={`/elections/${props.match.params.id}/edit`}>
+            <Button variant="light">Edit election</Button>
+          </Link>
+
+          <Button onClick={props.onCloseElection}>End voting</Button>
+        </React.Fragment>
       }
 
       <Button variant="danger" className="rounded mx-1" onClick={props.onDestroy}>Delete election</Button>
